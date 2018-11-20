1d490e2947c12f2529a4f1a6649e7198031ffb86
Eneko Amieva <enekoamieva@gmail.com>
2018-11-20 09:11:42 +0100

    Update IOS status bar with notch"

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //Registro de caches
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 //Registro de la estructura de la APP en un array para el cache STATIC
@@ -54,8 +54,13 @@ self.addEventListener('activate', event => {
     const deleteCaches = caches.keys().then(keys => {
         //Interactuamos con cada cache guardado
         keys.forEach(key => {
-            //Eliminamos el cache estatico que no se la última versión registrada
+            //Eliminamos el cache estatico que no se usa para tener la última versión registrada
             if( key != STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            //Eliminamos el cache dinámico que no se usa para tener la última versión registrada
+            if( key != DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
         });
